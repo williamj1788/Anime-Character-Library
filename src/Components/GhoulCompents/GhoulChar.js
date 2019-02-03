e81c05367c50ccb4e3a68967e95e8d56de77354d
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-03 11:42:30 -0500

    completed info container

--- src/Components/GhoulCompents/GhoulChar.js
+++ src/Components/GhoulCompents/GhoulChar.js
@@ -15,7 +15,28 @@ class GhoulChar extends React.Component{
                     <Kaneki />
                     <Kaneki />
                 </div>
-                
+                <div className="info-container">
+                    <div className="row">
+                        <p className="info-age title">Age:</p>
+                        <p className="info-age">Age</p>
+                    </div>
+                    <div className="row">
+                        <p className="info-age title">Power:</p>
+                        <p className="info-age">Power</p>
+                    </div>
+                    <p className="info-history">History</p>
+                    <p className="info-history-text">Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit. Aenean id nisi sed est dictum pretium. 
+                    Ut justo urna, hendrerit suscipit imperdiet vitae, tincidunt laoreet 
+                    enim. Aenean sit amet lorem in elit hendrerit suscipit vel quis purus.
+                    Nullam eget sem malesuada, maximus arcu quis, faucibus orci. Mauris 
+                    ultrices magna libero, id imperdiet ipsum viverra nec. Donec finibus
+                    massa diam, id ornare nulla accumsan sit amet. Sed in interdum eros.
+                    Fusce id elit at mauris placerat luctus in vel leo. Proin bibendum
+                    nunc mi, facilisis ullamcorper elit porta non. Sed convallis, nunc
+                    non aliquam tristique, ante elit varius sapien, in sagittis eros.</p>
+                </div>
+                <div style={{height: "1px"}}></div>
             </div>
         )
     }

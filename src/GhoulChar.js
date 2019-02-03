98ddff743b9f6a04dccc0f52b3a9248bd0c72917
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-03 10:16:33 -0500

    Complete card Temeplate

--- src/GhoulChar.js
+++ src/GhoulChar.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 class GhoulChar extends React.Component{
 
-    
     render(){
         return(
             <div id="Ghoul-container">
                 <div className="char-container">
-                    <div>Character</div>
-                    <div>Character</div>
-                    <div>Character</div>
-                    <div>Character</div>
-                    <div>Character</div>
-                    <div>Character</div>
-                
+                    <div className="card">
+                        <div className="card-header">
+                            <h5>Character</h5>
+                        </div>
+                        <div className="card-body">
+                            <img src="Kaneki.jpg" width="100%" height="100%"></img>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
